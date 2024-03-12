bc25d2b9bdc3b4f837c3df1f04430db5d79167e1
dineshpanchal93 <dineshpanchal432@gmail.com>
2024-03-12 07:24:20 +0000

    .

---
+++ graceworks/graceworks/doctype/budget_master/budget_master.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Addon-s India and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Budget Master", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ graceworks/graceworks/doctype/budget_master/test_budget_master.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Addon-s India and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestBudgetMaster(FrappeTestCase):
+	pass
