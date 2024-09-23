9a95d1db288fd4d9292b1ac6215f0aefe4be2e8d
Dinesh Panchal <dineshpanchal432@gmail.com>
2024-09-23 11:45:59 +0530

    Update purchase_order.py

--- graceworks/purchase_order.py
+++ graceworks/purchase_order.py
@@ -1,22 +1,21 @@
-import frappe
+# import frappe
 
-def po_before_submit(doc, method):
-    # check if the custom_attach_file field is set or not if not then throw error and stop submission
-    if not doc.custom_attach_file:
-        frappe.throw("Please attach the file before submission")
+# def po_before_submit(doc, method):
+#     # check if the custom_attach_file field is set or not if not then throw error and stop submission
+#     if not doc.custom_attach_file:
+#         frappe.throw("Please attach the file before submission")
         
-    # if there is no attachment on image, custom_image_1 and custom_image_2 in items table then throw error and stop submission
-    # for item in doc.items:
-    #     if or item.image or not item.custom_image_1 or not item.custom_image_2:
-    #         frappe.throw("Please attach the image before submission")
+#     # if there is no attachment on image, custom_image_1 and custom_image_2 in items table then throw error and stop submission
+#     # for item in doc.items:
+#     #     if or item.image or not item.custom_image_1 or not item.custom_image_2:
+#     #         frappe.throw("Please attach the image before submission")
 
 
-def po_before_update(doc, method):
-    # check if the custom_attach_file field is set or not if not then throw error and stop submission
-    if doc.workflow_state == "Review":
-        if not doc.custom_attach_file:
-            frappe.throw("Please attach the file before submission")
-            # change the status to Pending
-            doc.workflow_state = "Pending"
-            doc.save()
-        
+# def po_before_update(doc, method):
+#     # check if the custom_attach_file field is set or not if not then throw error and stop submission
+#     if doc.workflow_state == "Review":
+#         if not doc.custom_attach_file:
+#             frappe.throw("Please attach the file before submission")
+#             # change the status to Pending
+#             doc.workflow_state = "Pending"
+#             doc.save()
